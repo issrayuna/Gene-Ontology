a738c5b428d8b991677fa26fb96dc4f523c0d722
issrayuna <issra.njah@live.de>
2020-02-23 17:24:44 +0100

    Add files via upload

--- runGO.java
+++ runGO.java
@@ -6,55 +6,34 @@ import java.io.IOException;
 public class runGO {
 
     public static void main(String[] args) {
-        Option obo = new Option("obo", "obo", true, "obo file path");
-        Option root = new Option("root", "root", true, "specifiy which namespace to use");
-        Option mapping = new Option("mapping", "mapping", true, "mapping file path");
-        Option mappingtype = new Option("mappingtype", "mappingtype", true, "mappingtype: ensemble or go");
-        Option overlapout = new Option("overlapout", "overlapout", true, "output of ");
-        Option enrich = new Option("enrich", "enrich", true, "simulation file path for enrichment analysis");
-        Option minsize = new Option("minsize", "minsize", true, "minimal number of overlapping genes");
-        Option maxsize = new Option("maxsize", "maxsize", true, "maximal number of overlapping genes");
-        Option o = new Option("o", "o", true, "output file path [tsv]");
-
-        obo.setRequired(true);
-        root.setRequired(true);
-        mapping.setRequired(true);
-        mappingtype.setRequired(true);
-        overlapout.setRequired(false);
-        enrich.setRequired(true);
-        minsize.setRequired(true);
-        maxsize.setRequired(true);
-        o.setRequired(true);
 
         Options options = new Options();
-        options.addOption(obo);
-        options.addOption(root);
-        options.addOption(mapping);
-        options.addOption(mappingtype);
-        options.addOption(overlapout);
-        options.addOption(enrich);
-        options.addOption(minsize);
-        options.addOption(minsize);
-        options.addOption(maxsize);
-        options.addOption(o);
 
+        options.addRequiredOption("obo", "obo", true, "obo file path");
+        options.addRequiredOption("root", "root", true, "specifiy which namespace to use");
+        options.addRequiredOption("mapping", "mapping", true, "mapping file path");
+        options.addRequiredOption("mappingtype", "mappingtype", true, "mappingtype: ensemble or go");
+        options.addOption("overlapout", "overlapout", true, "output of ");
+        options.addRequiredOption("enrich", "enrich", true, "simulation file path for enrichment analysis");
+        options.addRequiredOption("minsize", "minsize", true, "minimal number of overlapping genes");
+        options.addRequiredOption("maxsize", "maxsize", true, "maximal number of overlapping genes");
+        options.addRequiredOption("o", "o", true, "output file path [tsv]");
 
         try {
-            CommandLineParser parser = new DefaultParser();
-            CommandLine cmd = parser.parse(options, args);
-            String oboPath = cmd.getOptionValue("obo");
-            String rootOpt = cmd.getOptionValue("root");
-            String mappingFilePath = cmd.getOptionValue("mapping");
-            String mappingType = cmd.getOptionValue("mappingtype");
+
+            CommandLine cmd = new DefaultParser().parse(options, args);
             String overlapOut = cmd.getOptionValue("overlapout");
-            String enrichFile = cmd.getOptionValue("enrich");
-            int minSize = Integer.parseInt(cmd.getOptionValue("minsize"));
-            int maxSize = Integer.parseInt(cmd.getOptionValue("maxsize"));
-            String outPath = cmd.getOptionValue("o");
 
-
-            reader.runOverlapAndEnrichmentAnalysis(outPath, oboPath, mappingFilePath, mappingType,
-                    rootOpt, enrichFile, minSize, maxSize, overlapOut);
+            reader.runOverlapAndEnrichmentAnalysis(
+                    cmd.getOptionValue("o"),
+                    cmd.getOptionValue("obo"),
+                    cmd.getOptionValue("mapping"),
+                    cmd.getOptionValue("mappingtype"),
+                    cmd.getOptionValue("root"),
+                    cmd.getOptionValue("enrich"),
+                    Integer.parseInt(cmd.getOptionValue("minsize")),
+                    Integer.parseInt(cmd.getOptionValue("maxsize")),
+                    overlapOut);
 
 
         } catch (ParseException | IOException pe) {
